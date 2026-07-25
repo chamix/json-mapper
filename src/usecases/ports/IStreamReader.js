c724e42a8989c879fd256e407ff685111f912f48
chamix <vera.camilocsharp@gmail.com>
2026-07-25 13:51:20 -0300

    test: complete harness certification (fault-injection validated all hook paths)

--- src/usecases/ports/IStreamReader.js
+++ src/usecases/ports/IStreamReader.js
@@ -1,3 +1,4 @@
+// Port boundary: stream input abstraction (see functional_domain.md §6.1)
 export class IStreamReader {
   async readJson(stream) {
     throw new Error('Method "readJson(stream)" must be implemented.');
